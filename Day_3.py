b37a7c8e0a945fff4e8f260a1774c0ec24ae5d41
AMRITA MALIK <codewithamrita@gmail.com>
2026-02-06 12:53:47 +0530

    Create Day_3.py

---
+++ Day_3.py
@@ -0,0 +1,27 @@
+#This is a program for assignment operator
+a=float(input("Enter the value of a ="))
+b=float(input("Enter the value of b ="))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+Division=a/b
+modulus=a%b
+floor_division=a//b
+power=a**b
+print("Addition",addition,"\nSubtraction",subtraction,"\nMultiplication:",multiplication,"\nDivision:",Division,"\nModulus",modulus,"\nFloor division:",floor_division,"\nPower",power)
+OR
+a=7
+b=6
+print("Addition is :",a+b,"\nDifference is:",a-b,")
+
+#This is a program for rcomparison operators
+a=float(input("Enter the value of a :"))
+b=float(input("Enter the value of b :"))
+print((a==b),(a!=b),(a>b),(a<b),(a>=b),(a<=b))
+
+#This is a program for logical operators
+a=float(input("Enter the value of a ="))
+b=float(input("Enter the value of b ="))
+print(a>b and b<a)
+print(a>b or b<a)
+print(not(a>b))
